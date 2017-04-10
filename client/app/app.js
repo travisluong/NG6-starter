4149625c8b53f6e0b496f904e6cd5ad4d1dad39f
Travis Luong <travis@travisluong.com>
2017-04-10 14:41:11 -0700

    Async actions

--- client/app/app.js
+++ client/app/app.js
@@ -4,10 +4,11 @@ import Common from './common/common';
 import Components from './components/components';
 import AppComponent from './app.component';
 import 'normalize.css';
-// import reducers from './reducers';
+import reducers from './reducers';
 import { combineReducers } from 'redux';
 // import loggingMiddleware from './loggingMiddleware';
 import ngRedux from 'ng-redux';
+import thunk from 'redux-thunk';
 
 angular.module('app', [
     uiRouter,
@@ -21,9 +22,9 @@ angular.module('app', [
     // #how-to-configure-your-server-to-work-with-html5mode
     $locationProvider.html5Mode(true).hashPrefix('!');
 
-    $ngReduxProvider.createStoreWith({
-      reducer: function(state, action){}
-    }, ['promiseMiddleware']);
+    // let reducer = combineReducers(reducers);
+
+    $ngReduxProvider.createStoreWith(reducers, [thunk]);
   })
 
   .component('app', AppComponent);
--- client/app/components/example/example.controller.js
+++ client/app/components/example/example.controller.js
@@ -1,6 +1,19 @@
+import * as CounterActions from '../../actions/counter';
+
 class ExampleController {
-  constructor() {
+  constructor($ngRedux, $scope) {
+    "ngInject";
     this.name = 'example';
+
+    const unsubscribe = $ngRedux.connect(this.mapStateToThis, CounterActions)(this);
+    $scope.$on('$destroy', unsubscribe);
+  }
+
+  // Which part of the Redux global state does our component want to receive?
+  mapStateToThis(state) {
+    return {
+      value: state.counter
+    }
   }
 }
 
